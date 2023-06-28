c04d37eb1f03abb40fb67c8368d90e709477f7f5
Zoe Agustina Tira <zoeagustinatira@gmail.com>
2023-06-27 21:59:41 -0300

    add dataSourceName

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,11 @@ import (
 	"database/sql"
 	"github.com/ZoeAgustinatira/DentalOffice/cmd/server/routes"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, _ := sql.Open("mysql", "sarasa")
+	db, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/dentaloffice")
 	eng := gin.Default()
 
 	router := routes.NewRouter(eng, db)
